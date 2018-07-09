c668e0769edfc998b6b0f4c6f1cd29db882b466d
rachel1032 <racheles32@gmail.com>
2018-07-09 18:57:18 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -3,7 +3,7 @@
 
 your_favorite_word = "food"
 
-puts "My favorite word is #{your_favorite_word}".upcase
+puts "My favorite word is #{your_favorite_word}".capitalize
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
